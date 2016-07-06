bb35da8836cb88146c8f91a5db7fd042043cc0e9
ezequielsd <ezequielsd@hotmail.com>
2016-07-06 13:33:39 -0300

    Primeira versão.

---
+++ FoldersCompare/FormMain.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace FoldersCompare
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.listViewDireita = new System.Windows.Forms.ListView();
+            this.listViewEsquerda = new System.Windows.Forms.ListView();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblArquivosFaltantesDireitaEsquerda = new System.Windows.Forms.Label();
+            this.lblArquivosFaltantesEsquerdaDireita = new System.Windows.Forms.Label();
+            this.btnCompareDireitaEsquerda = new System.Windows.Forms.Button();
+            this.lblQuantidadeItensDireita = new System.Windows.Forms.Label();
+            this.lblQuantidadeItensEsquerda = new System.Windows.Forms.Label();
+            this.btnCompareEsquerdaDireita = new System.Windows.Forms.Button();
+            this.txtPathDireira = new System.Windows.Forms.TextBox();
+            this.txtPathEsquerda = new System.Windows.Forms.TextBox();
+            this.btnEsquerda = new System.Windows.Forms.Button();
+            this.btnDireita = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listViewDireita
+            // 
+            this.listViewDireita.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listViewDireita.Location = new System.Drawing.Point(0, 0);
+            this.listViewDireita.Name = "listViewDireita";
+            this.listViewDireita.Size = new System.Drawing.Size(692, 659);
+            this.listViewDireita.Sorting = System.Windows.Forms.SortOrder.Ascending;
+            this.listViewDireita.TabIndex = 0;
+            this.listViewDireita.UseCompatibleStateImageBehavior = false;
+            this.listViewDireita.View = System.Windows.Forms.View.List;
+            // 
+            // listViewEsquerda
+            // 
+            this.listViewEsquerda.AutoArrange = false;
+            this.listViewEsquerda.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listViewEsquerda.GridLines = true;
+            this.listViewEsquerda.Location = new System.Drawing.Point(0, 0);
+            this.listViewEsquerda.Name = "listViewEsquerda";
+            this.listViewEsquerda.Size = new System.Drawing.Size(681, 659);
+            this.listViewEsquerda.TabIndex = 0;
+            this.listViewEsquerda.UseCompatibleStateImageBehavior = false;
+            this.listViewEsquerda.View = System.Windows.Forms.View.List;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.splitContainer1.IsSplitterFixed = true;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 78);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.listViewEsquerda);
+            this.splitContainer1.Panel1MinSize = 50;
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.listViewDireita);
+            this.splitContainer1.Size = new System.Drawing.Size(1377, 659);
+            this.splitContainer1.SplitterDistance = 681;
+            this.splitContainer1.TabIndex = 7;
+            // 
+            // panel1
+            // 
+            this.panel1.AutoSize = true;
+            this.panel1.Controls.Add(this.lblArquivosFaltantesDireitaEsquerda);
+            this.panel1.Controls.Add(this.lblArquivosFaltantesEsquerdaDireita);
+            this.panel1.Controls.Add(this.btnCompareDireitaEsquerda);
+            this.panel1.Controls.Add(this.lblQuantidadeItensDireita);
+            this.panel1.Controls.Add(this.lblQuantidadeItensEsquerda);
+            this.panel1.Controls.Add(this.btnCompareEsquerdaDireita);
+            this.panel1.Controls.Add(this.txtPathDireira);
+            this.panel1.Controls.Add(this.txtPathEsquerda);
+            this.panel1.Controls.Add(this.btnEsquerda);
+            this.panel1.Controls.Add(this.btnDireita);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1377, 75);
+            this.panel1.TabIndex = 8;
+            // 
+            // lblArquivosFaltantesDireitaEsquerda
+            // 
+            this.lblArquivosFaltantesDireitaEsquerda.AutoSize = true;
+            this.lblArquivosFaltantesDireitaEsquerda.Location = new System.Drawing.Point(882, 31);
+            this.lblArquivosFaltantesDireitaEsquerda.Name = "lblArquivosFaltantesDireitaEsquerda";
+            this.lblArquivosFaltantesDireitaEsquerda.Size = new System.Drawing.Size(89, 13);
+            this.lblArquivosFaltantesDireitaEsquerda.TabIndex = 29;
+            this.lblArquivosFaltantesDireitaEsquerda.Text = "Arquivos listados.";
+            // 
+            // lblArquivosFaltantesEsquerdaDireita
+            // 
+            this.lblArquivosFaltantesEsquerdaDireita.AutoSize = true;
+            this.lblArquivosFaltantesEsquerdaDireita.Location = new System.Drawing.Point(245, 31);
+            this.lblArquivosFaltantesEsquerdaDireita.Name = "lblArquivosFaltantesEsquerdaDireita";
+            this.lblArquivosFaltantesEsquerdaDireita.Size = new System.Drawing.Size(89, 13);
+            this.lblArquivosFaltantesEsquerdaDireita.TabIndex = 28;
+            this.lblArquivosFaltantesEsquerdaDireita.Text = "Arquivos listados.";
+            // 
+            // btnCompareDireitaEsquerda
+            // 
+            this.btnCompareDireitaEsquerda.Location = new System.Drawing.Point(705, 3);
+            this.btnCompareDireitaEsquerda.Name = "btnCompareDireitaEsquerda";
+            this.btnCompareDireitaEsquerda.Size = new System.Drawing.Size(98, 25);
+            this.btnCompareDireitaEsquerda.TabIndex = 27;
+            this.btnCompareDireitaEsquerda.Text = "<--  Compare ";
+            this.btnCompareDireitaEsquerda.UseVisualStyleBackColor = true;
+            this.btnCompareDireitaEsquerda.Click += new System.EventHandler(this.btnCompareDireitaEsquerda_Click);
+            // 
+            // lblQuantidadeItensDireita
+            // 
+            this.lblQuantidadeItensDireita.AutoSize = true;
+            this.lblQuantidadeItensDireita.Location = new System.Drawing.Point(1205, 31);
+            this.lblQuantidadeItensDireita.Name = "lblQuantidadeItensDireita";
+            this.lblQuantidadeItensDireita.Size = new System.Drawing.Size(89, 13);
+            this.lblQuantidadeItensDireita.TabIndex = 26;
+            this.lblQuantidadeItensDireita.Text = "Arquivos listados.";
+            // 
+            // lblQuantidadeItensEsquerda
+            // 
+            this.lblQuantidadeItensEsquerda.AutoSize = true;
+            this.lblQuantidadeItensEsquerda.Location = new System.Drawing.Point(1, 31);
+            this.lblQuantidadeItensEsquerda.Name = "lblQuantidadeItensEsquerda";
+            this.lblQuantidadeItensEsquerda.Size = new System.Drawing.Size(89, 13);
+            this.lblQuantidadeItensEsquerda.TabIndex = 25;
+            this.lblQuantidadeItensEsquerda.Text = "Arquivos listados.";
+            // 
+            // btnCompareEsquerdaDireita
+            // 
+            this.btnCompareEsquerdaDireita.Location = new System.Drawing.Point(543, 3);
+            this.btnCompareEsquerdaDireita.Name = "btnCompareEsquerdaDireita";
+            this.btnCompareEsquerdaDireita.Size = new System.Drawing.Size(98, 25);
+            this.btnCompareEsquerdaDireita.TabIndex = 24;
+            this.btnCompareEsquerdaDireita.Text = "Compare  -->";
+            this.btnCompareEsquerdaDireita.UseVisualStyleBackColor = true;
+            this.btnCompareEsquerdaDireita.Click += new System.EventHandler(this.btnCompareEsquerdaDireita_Click);
+            // 
+            // txtPathDireira
+            // 
+            this.txtPathDireira.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.txtPathDireira.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.HistoryList;
+            this.txtPathDireira.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtPathDireira.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPathDireira.Location = new System.Drawing.Point(685, 49);
+            this.txtPathDireira.Name = "txtPathDireira";
+            this.txtPathDireira.Size = new System.Drawing.Size(653, 21);
+            this.txtPathDireira.TabIndex = 23;
+            // 
+            // txtPathEsquerda
+            // 
+            this.txtPathEsquerda.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.txtPathEsquerda.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.HistoryList;
+            this.txtPathEsquerda.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtPathEsquerda.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPathEsquerda.Location = new System.Drawing.Point(0, 50);
+            this.txtPathEsquerda.Name = "txtPathEsquerda";
+            this.txtPathEsquerda.Size = new System.Drawing.Size(641, 21);
+            this.txtPathEsquerda.TabIndex = 22;
+            // 
+            // btnEsquerda
+            // 
+            this.btnEsquerda.Location = new System.Drawing.Point(640, 49);
+            this.btnEsquerda.Name = "btnEsquerda";
+            this.btnEsquerda.Size = new System.Drawing.Size(42, 23);
+            this.btnEsquerda.TabIndex = 21;
+            this.btnEsquerda.Text = "...";
+            this.btnEsquerda.UseVisualStyleBackColor = true;
+            this.btnEsquerda.Click += new System.EventHandler(this.btnEsquerda_Click);
+            // 
+            // btnDireita
+            // 
+            this.btnDireita.Location = new System.Drawing.Point(1337, 48);
+            this.btnDireita.Name = "btnDireita";
+            this.btnDireita.Size = new System.Drawing.Size(40, 23);
+            this.btnDireita.TabIndex = 20;
+            this.btnDireita.Text = "...";
+            this.btnDireita.UseVisualStyleBackColor = true;
+            this.btnDireita.Click += new System.EventHandler(this.btnDireita_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1377, 737);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.splitContainer1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Folder Compare";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.ListView listViewDireita;
+        private System.Windows.Forms.ListView listViewEsquerda;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnCompareDireitaEsquerda;
+        private System.Windows.Forms.Label lblQuantidadeItensDireita;
+        private System.Windows.Forms.Label lblQuantidadeItensEsquerda;
+        private System.Windows.Forms.Button btnCompareEsquerdaDireita;
+        private System.Windows.Forms.TextBox txtPathDireira;
+        private System.Windows.Forms.TextBox txtPathEsquerda;
+        private System.Windows.Forms.Button btnEsquerda;
+        private System.Windows.Forms.Button btnDireita;
+        private System.Windows.Forms.Label lblArquivosFaltantesDireitaEsquerda;
+        private System.Windows.Forms.Label lblArquivosFaltantesEsquerdaDireita;
+    }
+}
+
---
+++ FoldersCompare/FormMain.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FoldersCompare
+{
+    public partial class frmMain : Form
+    {
+        int countArquivosFaltantesDireita = 0;
+        int countArquivosFaltantesEsquerda = 0;
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        #region Eventos
+
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            //limpa campos
+            lblArquivosFaltantesEsquerdaDireita.Text = "";
+            lblArquivosFaltantesDireitaEsquerda.Text = "";            
+        }
+
+        /// <summary>
+        /// Evento botão seleciona Path na esquerda
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnEsquerda_Click(object sender, EventArgs e)
+        {
+            if (DialogResult.OK == folderBrowserDialog1.ShowDialog())
+            {
+                if (folderBrowserDialog1.SelectedPath != null)
+                {
+                    txtPathEsquerda.Text = folderBrowserDialog1.SelectedPath;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Evento botão seleciona Path na direita
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnDireita_Click(object sender, EventArgs e)
+        {
+            if(DialogResult.OK == folderBrowserDialog1.ShowDialog())
+            {
+                if (folderBrowserDialog1.SelectedPath != null)
+                {
+                    txtPathDireira.Text = folderBrowserDialog1.SelectedPath;
+                }
+            }            
+        }
+
+        /// <summary>
+        /// Evento botão comparar da esquerda para direita
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnCompareEsquerdaDireita_Click(object sender, EventArgs e)
+        {
+            //limpa campos
+            lblArquivosFaltantesEsquerdaDireita.Text = "";
+            lblArquivosFaltantesDireitaEsquerda.Text = "";
+            countArquivosFaltantesDireita = 0;
+            countArquivosFaltantesEsquerda = 0;
+
+            CompareEsquerdaDireita(txtPathEsquerda.Text, txtPathDireira.Text);
+        }
+
+        /// <summary>
+        /// Evento botão comparar da direita para esquerda
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnCompareDireitaEsquerda_Click(object sender, EventArgs e)
+        {
+            //limpa campos
+            lblArquivosFaltantesEsquerdaDireita.Text = "";
+            lblArquivosFaltantesDireitaEsquerda.Text = "";
+            countArquivosFaltantesDireita = 0;
+            countArquivosFaltantesEsquerda = 0;
+
+            CompareDireitaEsquerda(txtPathEsquerda.Text, txtPathDireira.Text);
+        }
+
+
+        #endregion
+
+        #region Métodos
+
+        /// <summary>
+        /// Método compara da esquerda para direita
+        /// </summary>
+        /// <param name="pathEsquerda">localizacao dos arquivos da esquerda</param>
+        /// <param name="pathDireita">localizacao dos arquivos da direita</param>
+        private void CompareEsquerdaDireita(string pathEsquerda, string pathDireita)
+        {  
+            listViewEsquerda.Clear();
+            listViewDireita.Clear();
+
+            PreencherListViewEsquerda(pathEsquerda);
+            PreencherListViewDireita(pathDireita);
+
+            //Comparação da esquerda para direita
+            for (int i = 0; i < listViewEsquerda.Items.Count - 1; i++)
+            {
+                FileInfo fileEsquerda = new FileInfo(listViewEsquerda.Items[i].Text);
+                bool contem = false;
+
+                foreach (ListViewItem item in listViewDireita.Items)
+                {
+                    FileInfo fileDireita = new FileInfo(item.Text);
+
+                    if (String.Equals(fileEsquerda.Name, fileDireita.Name, StringComparison.CurrentCultureIgnoreCase))
+                    {
+                        contem = true;
+                        break;
+                    }
+                }
+
+                if (!contem)
+                {
+                    listViewEsquerda.Items[i].BackColor = Color.GreenYellow;
+                    countArquivosFaltantesDireita ++;
+                }
+            }
+
+            lblArquivosFaltantesEsquerdaDireita.Text = "Existem " + countArquivosFaltantesDireita + " arquivos a mais do que o lado direito.";
+        }
+
+        /// <summary>
+        /// Método compara da direita para esquerda
+        /// </summary>
+        /// <param name="pathEsquerda">localizacao dos arquivos da esquerda</param>
+        /// <param name="pathDireita">localizacao dos arquivos da direita</param>
+        private void CompareDireitaEsquerda(string pathEsquerda, string pathDireita)
+        {
+            listViewEsquerda.Clear();
+            listViewDireita.Clear();
+
+            PreencherListViewEsquerda(pathEsquerda);
+
+            PreencherListViewDireita(pathDireita);
+
+
+            //Comparação da esquerda para direita
+            for (int i = 0; i < listViewDireita.Items.Count - 1; i++)
+            {
+                FileInfo fileEsquerda = new FileInfo(listViewDireita.Items[i].Text);
+                bool contem = false;
+
+                foreach (ListViewItem item in listViewEsquerda.Items)
+                {
+                    FileInfo fileDireita = new FileInfo(item.Text);
+
+                    if (String.Equals(fileDireita.Name, fileEsquerda.Name, StringComparison.CurrentCultureIgnoreCase))
+                    {
+                        contem = true;
+                        break;
+                    }
+                }
+
+                if (!contem)
+                {
+                    listViewDireita.Items[i].BackColor = Color.GreenYellow;
+                    countArquivosFaltantesEsquerda++;
+                }
+            }
+
+            lblArquivosFaltantesDireitaEsquerda.Text = "Existem " + countArquivosFaltantesEsquerda + " arquivos a mais do que o lado esquerdo.";
+        }
+
+        /// <summary>
+        /// Método preencher o listView da esquerda
+        /// </summary>
+        /// <param name="pathEsquerda">localizacao dos arquivos da esquerda</param>
+        private void PreencherListViewEsquerda(string pathEsquerda)
+        {
+            string[] filePathsEsquerda;
+
+            //verifica se o diretorio da esquerda existe
+            if (Directory.Exists(pathEsquerda))
+            {
+                //pega a lista de arquivos e pastas
+                filePathsEsquerda = Directory.GetFiles(pathEsquerda, "*.*", SearchOption.AllDirectories);
+                Array.Sort(filePathsEsquerda);
+
+                //informa a quantidade de arquivos
+                lblQuantidadeItensEsquerda.Text = filePathsEsquerda.Count().ToString() + " arquivos listados.";
+
+                //adiciona uma coluna na listview
+                listViewEsquerda.View = View.Details;
+                listViewEsquerda.Columns.Add("Arquivo", 800, HorizontalAlignment.Left);
+
+
+                //percorre cada item e carrega no listbox
+                foreach (string fileEsquerda in filePathsEsquerda)
+                {
+                    string file = fileEsquerda.Replace(pathEsquerda, "");
+                    listViewEsquerda.Items.Add(file);
+                }
+
+            }
+        }
+
+        /// <summary>
+        /// Método preencher o listView da direita
+        /// </summary>
+        /// <param name="pathDireita">localizacao dos arquivos da direita</param>
+        private void PreencherListViewDireita(string pathDireita)
+        {
+            string[] filePathsDireita;
+
+            //verifica se o diretorio da direita existe
+            if (Directory.Exists(pathDireita))
+            {
+                //pega a lista de arquivos e pastas
+                filePathsDireita = Directory.GetFiles(pathDireita, "*.*", SearchOption.AllDirectories);
+                Array.Sort(filePathsDireita);
+
+                //informa a quantidade de arquivos
+                lblQuantidadeItensDireita.Text = filePathsDireita.Count().ToString() + " arquivos listados.";
+
+                //adiciona uma coluna na listview
+                listViewDireita.View = View.Details;
+                listViewDireita.Columns.Add("Arquivo", 800, HorizontalAlignment.Left);
+
+                //percorre cada item e carrega no listbox
+                foreach (string fileDireita in filePathsDireita)
+                {
+                    string file = fileDireita.Replace(pathDireita, "");
+                    listViewDireita.Items.Add(file);                    
+                }
+            }
+        }
+
+
+        #endregion
+
+       
+    }
+}
